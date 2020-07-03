4b31f294acf4eb5a66a40a17e86deb723338bae4
potzenhotz <lukasmuessle@gmail.com>
2020-07-03 19:45:01 +0200

    ongoing changes

--- weather_stats/app.py
+++ weather_stats/app.py
@@ -1,67 +1,45 @@
-import pandas as pd
 import os
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.express as px
+import plotly.graph_objects as go
+import weather_data as wd
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-df_weather_daily = pd.read_csv(
-    f"{dir_path}/data/produkt_klima_tag_19350101_20191231_01303.txt", sep=";"
-)
-# Remove whitespaces from header
-df_weather_daily = df_weather_daily.rename(columns=lambda x: x.strip())
 
-df_weather_daily["MESS_DATUM"] = pd.to_datetime(
-    df_weather_daily["MESS_DATUM"], format="%Y%m%d"
-)
-df_weather_daily["year"] = pd.DatetimeIndex(df_weather_daily["MESS_DATUM"]).year
-df_weather_daily["month"] = pd.DatetimeIndex(df_weather_daily["MESS_DATUM"]).month
-df_weather_daily["day"] = pd.DatetimeIndex(df_weather_daily["MESS_DATUM"]).day
-df_weather_daily["period"] = (
-    df_weather_daily["month"].astype(str) + "-" + df_weather_daily["day"].astype(str)
-)
-# df_temp_daily = df_weather_daily.pivot(index="period", columns="year", values="TMK")
+ws = wd.weather_station()
+# df_weather = ws.get_weather_df()
+df_temp = ws.get_temp_df()
 
-df_test = df_weather_daily[(df_weather_daily["year"].astype(str).str.contains("197"))]
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-print(df_test.head(50))
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+years_list = df_temp.columns.tolist()
+years_list.pop(0)
 
-app = dash.Dash(__name__)
+# fig = px.line(df_temp, x="period", y=2000,)
+# fig.add_scatter(df_temp, x="period", y=2001, mode="lines")
+# fig.update_layout(xaxis=dict(tickformat="%d-%b"))
+# TODO USE pandas melt to create better DF for plotting
+# https://stackoverflow.com/questions/58142058/how-to-plot-multiple-lines-on-the-same-y-axis-using-plotly-express-in-python
+fig = go.Figure()
+for year in years_list:
+    fig.add_trace(
+        go.Scatter(x=df_temp["period"], y=df_temp[year], mode="lines", name=year,)
+    )
+
 app.layout = html.Div(
     children=[
-        html.H2("Dashboard - Weather Stats"),
-        html.P("Visualising weather data with Plotly - Dash"),
+        html.H1(children="Hello Dash"),
         html.Div(
-            className="row",
-            children=[
-                html.Div(className="four columns div-user-controls"),
-                html.Div(
-                    className="eight columns div-for-charts bg-grey",
-                    children=[
-                        dcc.Graph(
-                            id="timeseries",
-                            config={"displayModeBar": False},
-                            animate=True,
-                            figure=px.line(
-                                df_test,
-                                x="period",
-                                y="TMK",
-                                color="year",
-                                template="plotly_dark",
-                            ).update_layout(
-                                {
-                                    "plot_bgcolor": "rgba(0, 0, 0, 0)",
-                                    "paper_bgcolor": "rgba(0, 0, 0, 0)",
-                                }
-                            ),
-                        )
-                    ],
-                ),
-            ],
+            children="""
+        Dash: A web application framework for Python.
+    """
         ),
+        dcc.Graph(id="example-graph", figure=fig),
     ]
 )
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
---
+++ weather_stats/weather_data.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import os
+import sys
+
+
+class weather_station:
+    def __init__(self, station: str = "Essen"):
+        self.station = station
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    def read_csv(self):
+        self.df_weather_data = pd.read_csv(
+            f"{self.dir_path}/data/produkt_klima_tag_19350101_20191231_01303.txt",
+            sep=";",
+        )
+
+    def transform_weather_data(self):
+        # Remove whitespaces from header
+        self.df_weather_data = self.df_weather_data.rename(columns=lambda x: x.strip())
+        self.df_weather_data["MESS_DATUM"] = pd.to_datetime(
+            self.df_weather_data["MESS_DATUM"], format="%Y%m%d"
+        )
+        self.df_weather_data["year"] = pd.DatetimeIndex(
+            self.df_weather_data["MESS_DATUM"]
+        ).year
+        self.df_weather_data["month"] = pd.DatetimeIndex(
+            self.df_weather_data["MESS_DATUM"]
+        ).month
+        self.df_weather_data["day"] = pd.DatetimeIndex(
+            self.df_weather_data["MESS_DATUM"]
+        ).day
+        self.df_weather_data["period"] = "2000-" + self.df_weather_data[
+            "MESS_DATUM"
+        ].dt.strftime("%m-%d")
+
+    def create_temp_df(self):
+        self.create_weather_df()
+        self.df_temp = self.df_weather_data.pivot(
+            index="period", columns="year", values="TMK"
+        ).reset_index()
+
+    def create_weather_df(self, testing: bool = False):
+        self.read_csv()
+        self.transform_weather_data()
+
+    def get_weather_df(self, testing: bool = False):
+        self.create_weather_df()
+        if testing:
+            return self.df_weather_data[
+                (self.df_weather_data["year"].astype(str).str.contains("197"))
+            ]
+        else:
+            return self.df_weather_data
+
+    def get_temp_df(self, testing: bool = False):
+        self.create_temp_df()
+        if testing:
+            return self.df_temp[["period", 1970, 1971, 1950, 2010]].copy()
+        else:
+            return self.df_temp
+
+
+if __name__ == "__main__":
+    ws = weather_station()
+    # df_weather = ws.get_weather_df(testing=True)
+    # print(df_weather.head(40))
+    df_temp = ws.get_temp_df(testing=True)
+    # print(df_temp.head(40))
