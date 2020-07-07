0e1a1d7c6e2eb171507602a1221903a9fcbbcf36
potzenhotz <lukasmuessle@gmail.com>
2020-07-07 17:42:33 +0200

    first step from documentation

--- weather_stats/app.py
+++ weather_stats/app.py
@@ -8,25 +8,27 @@ import weather_data as wd
 
 
 ws = wd.weather_station()
-# df_weather = ws.get_weather_df()
+df_weather = ws.get_weather_df()
 df_temp = ws.get_temp_df()
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-years_list = df_temp.columns.tolist()
-years_list.pop(0)
 
 # fig = px.line(df_temp, x="period", y=2000,)
 # fig.add_scatter(df_temp, x="period", y=2001, mode="lines")
-# fig.update_layout(xaxis=dict(tickformat="%d-%b"))
-# TODO USE pandas melt to create better DF for plotting
-# https://stackoverflow.com/questions/58142058/how-to-plot-multiple-lines-on-the-same-y-axis-using-plotly-express-in-python
-fig = go.Figure()
-for year in years_list:
-    fig.add_trace(
-        go.Scatter(x=df_temp["period"], y=df_temp[year], mode="lines", name=year,)
-    )
+
+fig = px.line(df_weather, x="period", y="PM", color="year")
+fig.update_layout(xaxis=dict(tickformat="%d-%b"))
+
+# years_list = df_temp.columns.tolist()
+# years_list.pop(0)
+# fig = go.Figure()
+# for year in years_list:
+#    fig.add_trace(
+#        go.Scatter(x=df_temp["period"], y=df_temp[year], mode="lines", name=year,)
+#    )
+
 
 app.layout = html.Div(
     children=[
