599af677f3993ff5f18b1e7e6e43369803d6a0c3
potzenhotz <lukasmuessle@gmail.com>
2020-07-08 08:39:38 +0200

    added new plots

--- weather_stats/app.py
+++ weather_stats/app.py
@@ -8,8 +8,8 @@ import weather_data as wd
 
 
 ws = wd.weather_station()
-df_weather = ws.get_weather_df()
-df_temp = ws.get_temp_df()
+df_weather = ws.get_weather_df(testing=True)
+# df_temp = ws.get_temp_df()
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
@@ -18,9 +18,21 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # fig = px.line(df_temp, x="period", y=2000,)
 # fig.add_scatter(df_temp, x="period", y=2001, mode="lines")
 
-fig = px.line(df_weather, x="period", y="PM", color="year")
-fig.update_layout(xaxis=dict(tickformat="%d-%b"))
+fig_temp = px.line(df_weather, x="period", y="TMK", color="year", title="Temperature")
+fig_temp.update_layout(xaxis=dict(tickformat="%d-%b"))
 
+fig_press = px.line(df_weather, x="period", y="PM", color="year", title="Pressure")
+fig_press.update_layout(xaxis=dict(tickformat="%d-%b"))
+
+fig_precip = px.line(
+    df_weather, x="period", y="RSK", color="year", title="Precipitation"
+)
+fig_precip.update_layout(xaxis=dict(tickformat="%d-%b"))
+
+fig_cover = px.line(
+    df_weather, x="period", y="NM", color="year", title="Cloud Coverage"
+)
+fig_cover.update_layout(xaxis=dict(tickformat="%d-%b"))
 # years_list = df_temp.columns.tolist()
 # years_list.pop(0)
 # fig = go.Figure()
@@ -38,7 +50,10 @@ app.layout = html.Div(
         Dash: A web application framework for Python.
     """
         ),
-        dcc.Graph(id="example-graph", figure=fig),
+        dcc.Graph(id="temp_graph", figure=fig_temp),
+        dcc.Graph(id="press_graph", figure=fig_press),
+        dcc.Graph(id="precip_graph", figure=fig_precip),
+        dcc.Graph(id="cover_graph", figure=fig_cover),
     ]
 )
 
